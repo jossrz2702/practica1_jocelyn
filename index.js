b9bee96048733b0c6d948dbab5628e368387df4c
jossrz2702 <89814279+jossrz2702@users.noreply.github.com>
2021-09-06 17:43:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+fuction get IMC() {
+  var peso=parsefloat(document.getElementById('peso').value);
+   var altura=parsefloat(document.getElementById('altura').value);
+  get ElementById
+  var imc=peso/(altura * altura);
+  document.getElementById("imc").innerHTML
